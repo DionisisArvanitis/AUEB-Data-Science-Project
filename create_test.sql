b47174e8339e3fea053f45420db7c92683fd8a86
DionisisArvanitis <dionisisarvanitis@protonmail.com>
2022-04-30 12:32:19 +0300

    1st commit

---
+++ create_test.sql
@@ -0,0 +1,16 @@
+create table Test(
+   season int,
+   yr int,
+   mnth int,
+   hr int,
+   holiday int,
+   weekday int,
+   workingday int,
+   weathersit int,
+   temp float,
+   atemp float,
+   hum float,
+   windspeed float,
+   casual int,
+   registered int
+);
---
+++ create_train.sql
@@ -0,0 +1,17 @@
+create table Train(
+   season int,
+   yr int,
+   mnth int,
+   hr int,
+   holiday int,
+   weekday int,
+   workingday int,
+   weathersit int,
+   temp float,
+   atemp float,
+   hum float,
+   windspeed float,
+   casual int,
+   registered int,
+   cnt int
+);
